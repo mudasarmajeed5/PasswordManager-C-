1e4d0ff9d68706537ecc735d4c998e7e5c60e117
mudasarmajeed5 <143939048+mudasarmajeed5@users.noreply.github.com>
2025-01-03 20:29:18 +0500

    Final PF Project Semester1

--- EndSemesterProject.cpp
+++ EndSemesterProject.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-using namespace std;
 #include <fstream>
 #include <string>
-// global variables
-struct userDetails{
+using namespace std;
+#include <random>
+#include <cctype>
+// Struct containing fullName of user and MasterPassword.
+struct userDetails
+{
 	string firstName;
 	string lastName;
 	string masterPassword;
-	string fileName;
 };
-string firstName, lastName, masterPassword, fileName;
+userDetails currentUser;
+// global variables
+string fileName;
 string html_doc;
 bool responseCheck();
-bool checkPassword(string masterKey);
+bool checkPassword(const string &masterKey);
 bool createLogin();
 string generatePassword();
 bool checkLogin();
@@ -26,14 +30,15 @@ bool fileIsEmpty(const string &filename)
 bool generateHtmlDoc(ofstream &myDoc);
 bool setIdentity()
 {
+	userDetails fileUser;
+	string file_name;
 	bool validMasterKey;
 	bool response = false, fileExists = false;
-	string first_name, last_name, master_password, file_name;
 	cout << "Create an account! : \n";
 	while (true)
 	{
 		cout << "Enter your first Name: (without spaces): ";
-		cin >> first_name;
+		cin >> fileUser.firstName;
 		cout << "\nConfirm?: ";
 		response = responseCheck();
 		if (response == true)
@@ -44,7 +49,7 @@ bool setIdentity()
 	while (true)
 	{
 		cout << "Enter your last Name: (without spaces): ";
-		cin >> last_name;
+		cin >> fileUser.lastName;
 		cout << "\nConfirm?: ";
 		response = responseCheck();
 		if (response == true)
@@ -55,32 +60,36 @@ bool setIdentity()
 	while (true)
 	{
 		cout << "\nEnter your MasterKey must be atleast 8 digits. ";
-		cin >> master_password;
-		validMasterKey = checkPassword(master_password);
+		cin >> fileUser.masterPassword;
+		validMasterKey = checkPassword(fileUser.masterPassword);
 		if (validMasterKey)
 		{
 			break;
 		}
 	}
 
-	for (int i = 0; i < first_name.length(); i++)
+	for (int i = 0; i < fileUser.firstName.length(); i++)
 	{
-		first_name[i] = tolower(first_name[i]);
+		fileUser.firstName[i] = tolower(fileUser.firstName[i]);
 	}
-	for (int i = 0; i < last_name.length(); i++)
+	for (int i = 0; i < fileUser.lastName.length(); i++)
 	{
-		last_name[i] = tolower(last_name[i]);
+		fileUser.lastName[i] = tolower(fileUser.lastName[i]);
 	}
-	file_name = first_name + last_name + ".txt";
-	html_doc = first_name + last_name + ".html";
+	// variables storng filenames of html document and .txt document that will be created to store the passwords in the file.
+	file_name = fileUser.firstName + fileUser.lastName + ".txt";
+	html_doc = fileUser.firstName + fileUser.lastName + ".html";
 	fileName = file_name;
+	// ofstream to allow read write operations in file.
 	ofstream myFile(file_name);
+	// check if there is no file. (Proper error handling, function will return if there is any error while creating the file.)
 	if (!myFile)
 	{
 		cout << "Error creating file.\n";
 		return false;
 	}
-	myFile << first_name << " " << last_name << " " << master_password << endl;
+	// Write into the file. The first name, last name and the master_password.
+	myFile << fileUser.firstName << " " << fileUser.lastName << " " << fileUser.masterPassword << endl;
 	myFile << "Website\t" << "Password" << endl;
 	myFile.close();
 	cout << "Your file has been created! ";
@@ -90,13 +99,13 @@ bool setIdentity()
 bool getIdentity()
 {
 	bool response = false, fileExists = false;
-
-	string first_name, last_name, master_password, file_name;
+	string file_name;
+	userDetails fileUser;
 	cout << "Sign In: \n";
 	while (true)
 	{
 		cout << "Enter your first Name: (without spaces): ";
-		cin >> first_name;
+		cin >> fileUser.firstName;
 		cout << "\nConfirm?: ";
 		response = responseCheck();
 		if (response == true)
@@ -107,7 +116,7 @@ bool getIdentity()
 	while (true)
 	{
 		cout << "Enter your last Name: (without spaces): ";
-		cin >> last_name;
+		cin >> fileUser.lastName;
 		cout << "\nConfirm?: ";
 		response = responseCheck();
 		if (response == true)
@@ -116,30 +125,30 @@ bool getIdentity()
 		}
 	};
 
-	for (int i = 0; i < first_name.length(); i++)
+	for (int i = 0; i < fileUser.firstName.length(); i++)
 	{
-		first_name[i] = tolower(first_name[i]);
+		fileUser.firstName[i] = tolower(fileUser.firstName[i]);
 	}
-	for (int i = 0; i < last_name.length(); i++)
+	for (int i = 0; i < fileUser.lastName.length(); i++)
 	{
-		last_name[i] = tolower(last_name[i]);
+		fileUser.lastName[i] = tolower(fileUser.lastName[i]);
 	}
-	file_name = first_name + last_name + ".txt";
+	file_name = fileUser.firstName + fileUser.lastName + ".txt";
 	fileName = file_name;
-	//	Store the data in the file if it exists:
 	ifstream readFile(file_name);
+
+	//	Read the first line in the file using ifstream if the file is opened.
 	if (readFile.is_open())
 	{
 		fileExists = true;
-		readFile >> firstName >> lastName >> masterPassword;
+		readFile >> currentUser.firstName >> currentUser.lastName >> currentUser.masterPassword;
 		readFile.close();
-		// validate with the userInput!
-		cout << firstName << endl;
-		//	if true authenticate user to view all passwords by returning true.
 	}
 	else
 	{
+		// if the file does not exist return false.
 		cout << "File does not exist.\n";
+		fileExists = false;
 	}
 	return fileExists;
 }
@@ -187,6 +196,7 @@ bool createLogin()
 		cout << "\nConfirmed siteName as: " << siteName << endl;
 
 		password = generatePassword();
+
 		if (response == true)
 		{
 			ofstream myFile(fileName, ios::app);
@@ -200,15 +210,27 @@ bool createLogin()
 		if (html_response)
 		{
 			ofstream myDoc(html_doc, ios::app);
+			// fileIsEmpty is a function that returns true if the file is empty and false if the file is not empty.
 			if (fileIsEmpty(html_doc))
 			{
 				generateHtmlDoc(myDoc);
 			}
-			myDoc << "<tr><td><a href='https://" << siteName
+			// append passwords in the form of html tables to save an html document for users.
+			myDoc << "<tr>"
+				  << "<td><a href='https://" << siteName
 				  << "' target='_blank'><i class='fas fa-external-link-alt'></i> "
-				  << siteName << "</a></td><td>"
-				  << password << "</td></tr>\n";
-			myDoc.close();
+				  << siteName << "</a></td>"
+				  << "<td>"
+				  << "<span class='password' style='display: none;'>" << password << "</span>"
+				  << "<input type='password' value='" << password
+				  << "' class='form-control d-inline-block password-input' readonly style='width: 70%;'>"
+				  << "<button class='btn btn-sm btn-secondary toggle-password'>"
+				  << "<i class='fas fa-eye'></i></button>"
+				  << "<button class='btn btn-sm btn-primary copy-password'>"
+				  << "<i class='fas fa-copy'></i></button>"
+				  << "</td>"
+				  << "</tr>\n";
+				  myDoc.close();
 		}
 		if (response != true)
 		{
@@ -225,6 +247,7 @@ bool responseCheck()
 		cout << "Enter 'Y' for Yes, 'N' for No, 'E' to exit: ";
 		cin >> response;
 		response = tolower(response);
+		// lowered the response entered by user to match the case.
 		switch (response)
 		{
 		case 'y':
@@ -249,57 +272,112 @@ bool responseCheck()
 		}
 	} while (true);
 };
-bool checkPassword(string masterKey)
+bool checkPassword(const string &masterKey)
 {
+	if (masterKey.length() < 8)
+	{
+		cout << "\nYour password must be at least 8 characters long.\n";
+		return false;
+	}
+	bool hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+	for (char ch : masterKey)
+	{
+		if (isupper(ch))
+			hasUpper = true;
+		else if (islower(ch))
+			hasLower = true;
+		else if (isdigit(ch))
+			hasDigit = true;
+		else
+			hasSpecial = true;
+	}
+	if (!hasUpper)
+		cout << "\nYour password must contain at least one uppercase letter.\n";
+	if (!hasLower)
+		cout << "\nYour password must contain at least one lowercase letter.\n";
+	if (!hasDigit)
+		cout << "\nYour password must contain at least one digit.\n";
+	if (!hasSpecial)
+		cout << "\nYour password must contain at least one special symbol (e.g., @, #, $, etc.).\n";
 
-	bool isValidKey = false;
-	if (masterKey.length() >= 8)
-	{
-		isValidKey = true;
-	}
-	else
-	{
-		isValidKey = false;
-		cout << "\nYour Password must be atleast 8 digits long";
-	}
-	return isValidKey;
-};
-string generatePassword()
-{	
-	string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-	string password = "";
-	int n = characters.length();
-
-	for (int i = 0; i < 12; ++i)
-	{
-		
-		int randIndex = rand() % n;
-		password += characters[randIndex];
-	}
-	cout<<"Password: "<<password<<endl;
-	return password;
+	return hasUpper && hasLower && hasDigit && hasSpecial;
 }
+bool isSecurePassword(const string &password)
+{
+	if (password.length() < 8)
+		return false;
+
+	bool hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+
+	for (char ch : password)
+	{
+		if (isupper(ch))
+			hasUpper = true;
+		else if (islower(ch))
+			hasLower = true;
+		else if (isdigit(ch))
+			hasDigit = true;
+		else
+			hasSpecial = true;
+	}
+
+	return hasUpper && hasLower && hasDigit && hasSpecial;
+}
+
+string generatePassword()
+{
+	//	predefined strucutre of passwords.
+	const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+	// create random password based on the time.
+	srand(static_cast<unsigned>(time(0)));
+	string password;
+	while (true)
+	{
+		password.clear(); // Clear any previous attempt.
+
+		for (int i = 0; i < 12; i++)
+		{
+			password += characters[rand() % characters.size()];
+		}
+
+		// Check if the generated password meets security requirements.
+		if (isSecurePassword(password))
+		{
+			cout << "Generated Secure Password: " << password << endl;
+			return password;
+		}
+	}
+}
+
 bool checkLogin()
 {
-	bool isAuthenticated = false, response;
+	bool isAuthenticated = false;
 	string masterKey;
 	while (true)
 	{
 		cout << "Enter your Master Password: ";
 		cin >> masterKey;
-		cout << "Confirm?\n";
-		response = responseCheck();
-		if (response == true)
+		if (masterKey == currentUser.masterPassword)
 		{
-			break;
+			isAuthenticated = true;
+			cout << "Authentication successful!\n";
+			break; // Exit the loop on successful authentication
+		}
+		else
+		{
+			cout << "Incorrect password. Try again.\n";
+		}
+
+		bool response = responseCheck();
+		if (!response)
+		{
+			cout << "Exiting login attempt.\n";
+			break; // Exit the loop if the user chooses not to retry
 		}
 	}
-	if (masterKey == masterPassword)
-	{
-		isAuthenticated = true;
-	}
 	return isAuthenticated;
-};
+}
+
 void viewPasswords()
 {
 	cout << "\nThese are your logins: \n";
@@ -320,7 +398,7 @@ void viewPasswords()
 	{
 		cout << "Error opening file." << endl;
 	}
-	cout<<"\n-------------------\nAdd Logins to your file\n";
+	cout << "\n-------------------\nAdd Logins to your file\n";
 	bool result = responseCheck();
 	if (result)
 	{
@@ -330,88 +408,114 @@ void viewPasswords()
 
 bool generateHtmlDoc(ofstream &myDoc)
 {
-	bool isSuccessful = false;
-	if (myDoc.is_open())
-	{
-		myDoc << "<!DOCTYPE html>\n"
-			  << "<html lang=\"en\">\n"
-			  << "<head>\n"
-			  << "<meta charset=\"UTF-8\">\n"
-			  << "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"
-			  << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
-			  << "<title>Password Manager</title>\n"
-			  << "<!-- Bootstrap CSS -->\n"
-			  << "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\">\n"
-			  << "<!-- Font Awesome for icons -->\n"
-			  << "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css\">\n"
-			  << "<style>\n"
-			  << "body {\n"
-			  << "    background-color: #1e1e2f;\n"
-			  << "    color: #e0e0e0;\n"
-			  << "    display: flex;\n"
-			  << "    justify-content: center;\n"
-			  << "    align-items: center;\n"
-			  << "    min-height: 100vh;\n"
-			  << "    margin: 0;\n"
-			  << "    font-family: 'Arial', sans-serif;\n"
-			  << "}\n"
-			  << "table {\n"
-			  << "    width: 70%;\n"
-			  << "    border-collapse: collapse;\n"
-			  << "    background-color: #2a2a3b;\n"
-			  << "    border-radius: 8px;\n"
-			  << "    overflow: hidden;\n"
-			  << "    animation: fadeIn 1s ease-in-out;\n"
-			  << "    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);\n"
-			  << "}\n"
-			  << "th, td {\n"
-			  << "    padding: 15px 20px;\n"
-			  << "    text-align: left;\n"
-			  << "}\n"
-			  << "th {\n"
-			  << "    background-color: #3b3b52;\n"
-			  << "    color: #ffffff;\n"
-			  << "    text-transform: uppercase;\n"
-			  << "    font-weight: bold;\n"
-			  << "}\n"
-			  << "tr:nth-child(even) {\n"
-			  << "    background-color: #33334d;\n"
-			  << "}\n"
-			  << "tr:hover {\n"
-			  << "    background-color: #444466;\n"
-			  << "    cursor: pointer;\n"
-			  << "    transition: background-color 0.3s;\n"
-			  << "}\n"
-			  << "a {\n"
-			  << "    color: #5dade2;\n"
-			  << "    text-decoration: none;\n"
-			  << "    font-weight: 600;\n"
-			  << "    display: flex;\n"
-			  << "    align-items: center;\n"
-			  << "}\n"
-			  << "a:hover {\n"
-			  << "    color: #76c7f2;\n"
-			  << "}\n"
-			  << "i {\n"
-			  << "    margin-right: 8px;\n"
-			  << "}\n"
-			  << "td:last-child {\n"
-			  << "    font-family: 'Courier New', Courier, monospace;\n"
-			  << "    font-weight: 500;\n"
-			  << "}\n"
-			  << "@keyframes fadeIn {\n"
-			  << "    from { opacity: 0; }\n"
-			  << "    to { opacity: 1; }\n"
-			  << "}\n"
-			  << "</style>\n"
-			  << "</head>\n"
-			  << "<body>\n"
-			  << "<table>\n"
-			  << "<thead>\n"
-			  << "<tr><th>Site Name</th><th>Password</th></tr>\n"
-			  << "</thead>\n";
+    bool isSuccessful = false;
 
-		isSuccessful = true;
-	}
-	return isSuccessful;
+    if (myDoc.is_open())
+    {
+        myDoc << "<!DOCTYPE html>\n"
+              << "<html lang=\"en\">\n"
+              << "<head>\n"
+              << "<meta charset=\"UTF-8\">\n"
+              << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+              << "<title>Password Manager</title>\n"
+              << "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\">\n"
+              << "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css\">\n"
+              << "<style>\n"
+              << "body {\n"
+              << "    background-color: #f8f9fa;\n"
+              << "    color: #212529;\n"
+              << "    display: flex;\n"
+              << "    justify-content: center;\n"
+              << "    align-items: start;\n"
+              << "    min-height: 100vh;\n"
+              << "    margin-top: 10vh;\n"
+              << "    font-family: 'Arial', sans-serif;\n"
+              << "}\n"
+              << "table {\n"
+              << "    width: 80%;\n"
+              << "    border-collapse: collapse;\n"
+              << "    background-color: #ffffff;\n"
+              << "    border: 1px solid #dee2e6;\n"
+              << "    border-radius: 8px;\n"
+              << "    overflow: hidden;\n"
+              << "    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);\n"
+              << "}\n"
+              << "th, td {\n"
+              << "    padding: 10px 15px;\n"
+              << "    text-align: center;\n"
+              << "}\n"
+              << "th {\n"
+              << "    background-color: #343a40;\n"
+              << "    color: #ffffff;\n"
+              << "    font-weight: bold;\n"
+              << "}\n"
+              << "tr:nth-child(even) {\n"
+              << "    background-color: #f1f3f5;\n"
+              << "}\n"
+              << "tr:hover {\n"
+              << "    background-color: #e9ecef;\n"
+              << "    cursor: pointer;\n"
+              << "}\n"
+              << ".toast-container {\n"
+              << "    position: fixed;\n"
+              << "    bottom: 1rem;\n"
+              << "    right: 1rem;\n"
+              << "}\n"
+              << "</style>\n"
+              << "</head>\n"
+              << "<body>\n"
+              << "<div class='toast-container'>\n"
+              << "    <div id='copy-toast' class='toast' role='alert' aria-live='assertive' aria-atomic='true'>\n"
+              << "        <div class='toast-header'>\n"
+              << "            <strong class='me-auto'>Notification</strong>\n"
+              << "            <small>Just now</small>\n"
+              << "            <button type='button' class='btn-close' data-bs-dismiss='toast' aria-label='Close'></button>\n"
+              << "        </div>\n"
+              << "        <div class='toast-body'>\n"
+              << "        </div>\n"
+              << "    </div>\n"
+              << "</div>\n"
+              << "<table>\n"
+              << "<thead>\n"
+              << "<tr><th>Site Name</th><th>Password</th><th>Actions</th></tr>\n"
+              << "</thead>\n";
+
+        myDoc << "<script>\n"
+              << "document.addEventListener('DOMContentLoaded', () => {\n"
+              << "    document.querySelectorAll('.toggle-password').forEach(button => {\n"
+              << "        button.addEventListener('click', () => {\n"
+              << "            const row = button.closest('tr');\n"
+              << "            const input = row.querySelector('.password-input');\n"
+              << "            if (input) {\n"
+              << "                if (input.type === 'password') {\n"
+              << "                    input.type = 'text';\n"
+              << "                    button.innerHTML = \"<i class='fas fa-eye-slash'></i>\";\n"
+              << "                } else {\n"
+              << "                    input.type = 'password';\n"
+              << "                    button.innerHTML = \"<i class='fas fa-eye'></i>\";\n"
+              << "                }\n"
+              << "            }\n"
+              << "        });\n"
+              << "    });\n"
+              << "\n"
+              << "    document.querySelectorAll('.copy-password').forEach(button => {\n"
+              << "        button.addEventListener('click', () => {\n"
+              << "            const row = button.closest('tr');\n"
+              << "            const input = row.querySelector('.password-input');\n"
+              << "            if (input) {\n"
+              << "                navigator.clipboard.writeText(input.value).then(() => {\n"
+              << "                    const toast = new bootstrap.Toast(document.getElementById('copy-toast'));\n"
+              << "                    document.querySelector('#copy-toast .toast-body').textContent = 'Password copied!';\n"
+              << "                    toast.show();\n"
+              << "                }).catch(err => console.error('Clipboard copy failed', err));\n"
+              << "            }\n"
+              << "        });\n"
+              << "    });\n"
+              << "});\n"
+              << "</script>\n";
+
+        isSuccessful = true;
+    }
+
+    return isSuccessful;
 }
